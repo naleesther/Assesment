ef51a6c71680ab866f64ca9bb3aa04d995698799
aluochvivian <aluochviviann@gmail.com>
2022-03-10 09:11:38 +0300

    first commit

---
+++ main.kt
@@ -0,0 +1,9 @@
+fun main(){
+  school()
+
+
+}
+fun nameSchool(){
+    var="codeHive"
+    println (name[4]().toString name[5]() name[6]()name[7]() name[8]())
+}
---
+++ src/main.kt
@@ -0,0 +1,25 @@
+fun main (){
+    println(nameOfSchool)
+    division(12 6)
+    println()
+    println(length)
+
+}
+fun nameOfSchool(name:String)
+    var ="codeHive"
+    println(name[4]().toString name[5]()name[6]()name[7]()
+}
+fun division(num1:Int,num2:Int):Int{
+    var =num1%num2
+    println(division)
+
+    fun nameAndAge(){
+        var= "my name is "  "and am 20 years old
+        var="Esther"
+
+        println("statement")
+
+    }
+}fun getlength(name:String)
+   var length="Kenya"
+   println(length)
